f3c5e3fc235e4427add5c654de37b9d332a60338
01mi01 <mijaelhaa@gmail.com>
2025-04-09 22:02:26 -0400

    Implemented user registration and login

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,73 @@
+const bcrypt = require('bcryptjs');
+const { User } = require('../models'); 
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const SECRET_KEY = process.env.SECRET_KEY;
+
+exports.registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    return res.status(400).json({ message: 'Llenar todos los campos' });
+  }
+
+  try {
+    const userExists = await User.findOne({ where: { email } });
+    if (userExists) {
+      return res.status(400).json({ message: 'Ya existe un usuario registrado' });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      message: 'Usuario registrado con éxito',
+      user: {
+        id: newUser.id,
+        name: newUser.name,
+        email: newUser.email,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error del servidor' });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: 'Email and password are required' });
+  }
+
+  try {
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return res.status(400).json({ message: 'Usuario no encontrado' });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: 'Contraseña incorrecta' });
+    }
+
+    const token = jwt.sign(
+      { id: user.id, email: user.email }, 
+      SECRET_KEY,
+      { expiresIn: process.env.TOKEN_EXPIRATION || '2h' } 
+    );
+
+    res.json({ token });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error del servidor' });
+  }
+};
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -9,6 +9,15 @@ const {
   deleteUser,
 } = require('../controllers/user.controller');
 
+const {
+  registerUser,
+  loginUser
+} = require('../controllers/auth.controller');
+
+router.post('/register', registerUser);
+
+router.post('/login', loginUser);
+
 router.get('/users', getUsers);
 
 router.post('/users', createUser);
